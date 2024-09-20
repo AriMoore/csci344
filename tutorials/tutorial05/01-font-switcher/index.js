338e4dbec02e7d2dfdc02c47ab3fddad73d02b31
Ari Moore <amoore18@unca.edu>
2024-09-20 14:47:07 -0400

    Tutorial 5

---
+++ tutorials/tutorial05/01-font-switcher/index.js
@@ -0,0 +1,9 @@
+const makeBigger = () => {
+   document.querySelector('h1').style.fontSize = "150px";
+   document.querySelector('p').style.fontSize = "150px";
+};
+
+const makeSmaller = () => {
+   document.querySelector('h1').style.fontSize = "10px";
+   document.querySelector('p').style.fontSize = "10px";
+};
